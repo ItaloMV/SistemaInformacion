6df2c563f4ab28a64c61d1559e58e4125010dba5
ItaloMV <italovalcarcel@gmail.com>
2021-10-01 21:52:31 -0500

    Nuevo Proyecto

---
+++ aplicacion/inicio.view.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<?php
+require 'functions.php';
+$permisos = ['Administrador','Profesor','Padre'];
+permisos($permisos);
+
+?>
+<html>
+<head>
+<title>Inicio | Registro de Notas</title>
+    <meta name="description" content="Registro de Notas del Centro Escolar Profesor Lennin" />
+    <link rel="stylesheet" href="css/style.css" />
+
+</head>
+<body>
+<div class="header">
+        <h1>Registro de Notas - "I.E.P. San Ignacio de Loyola"</h1>
+        <h3>Usuario:  <?php echo $_SESSION["username"] ?></h3>
+</div>
+<nav>
+    <ul>
+        <li class="active"><a href="inicio.view.php">Inicio</a> </li>
+        <li><a href="alumnos.view.php">Registro de Alumnos</a> </li>
+        <li><a href="listadoalumnos.view.php">Listado de Alumnos</a> </li>
+        <li><a href="notas.view.php">Registro de Notas</a> </li>
+        <li><a href="listadonotas.view.php">Consulta de Notas</a> </li>
+        <li class="right"><a href="logout.php">Salir</a> </li>
+
+    </ul>
+</nav>
+
+<div class="body">
+    <div class="panel">
+           <h1 class="text-center">"I.E.P. San Ignacio de Loyola"</h1>
+        <?php
+        if(isset($_GET['err'])){
+            echo '<h3 class="error text-center">ERROR: Usuario no autorizado</h3>';
+        }
+        ?>
+        <br>
+        <hr>
+        <p class="text-center"><strong>Integrantes GRUPO 1</strong><br><br>Matasoglio Valcarcel Italo Daniel<br><br>Pinedo Rubina Daniel Josias<br><br>Zecevich Vela Novak Milich</p>
+        <br>
+        </div>
+</div>
+
+<footer>
+
+    <p>Derechos reservados &copy; 2021</p>
+</footer>
+
+</body>
+
+</html>
